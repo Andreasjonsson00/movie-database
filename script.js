e3338d8ee84fed1d504a0868db93607d8f6c8b58
Andreas <andreasjonsson00@gmail.com>
2026-03-30 13:17:30 +0200

    add responsive design

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const API_KEY = "4b5b8bff36991cf98123acd013f7ae4f";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-const moviesDiv = document.getElementById("Movies");
+const moviesDiv = document.getElementById("moviesRow");
 
 async function fetchMovies() {
   try {
@@ -19,16 +19,24 @@ function displayMovies(movies) {
   moviesDiv.innerHTML = "";
   for (const movie of movies) {
     const movieElement = document.createElement("div");
-    movieElement.classList.add("movie", "p-2");
+    movieElement.classList.add(
+      "col-12",
+      "col-sm-6",
+      "col-md-4",
+      "col-lg-3",
+      "mb-4"
+    );
     movieElement.innerHTML = `
-    <div class="mt-2 border rounded shadow"> 
-    <a href="https://www.themoviedb.org/movie/${movie.id}" target="_blank">
-    <img class="shadow rounded" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-    </a>
-      <div class="p-2"> 
-      <h3>${movie.title}</h3>
-      <p>Release Date: ${movie.release_date}</p>
-      <p>Rating: ${movie.vote_average}</p>
+    <div class="mt-2 border rounded shadow h-100 d-flex flex-column"> 
+      <a href="https://www.themoviedb.org/movie/${movie.id}" target="_blank">
+        <img class="img-fluid shadow rounded" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+      </a>
+      <div class="p-2 flex-grow-1 d-flex flex-column justify-content-between"> 
+        <div>
+          <h5>${movie.title}</h5>
+          <p class="mb-1">Release Date: ${movie.release_date}</p>
+          <p class="mb-2">Rating: ${movie.vote_average}</p>
+        </div>
       </div>
     </div>
     `;
